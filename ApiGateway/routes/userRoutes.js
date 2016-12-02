be1fe3b91f66a4bf46b4ad6690d680c04ed08668
antoniomejias <tonyraptor@gmail.com>
2016-12-01 22:38:55 -0400

    improving Users Microservice

--- ApiGateway/routes/userRoutes.js
+++ ApiGateway/routes/userRoutes.js
@@ -1,44 +1,13 @@
-import {
-    Router
-}
-from 'express';
+import {Router} from 'express';
 import Studio from 'studio';
-
-// var Studio = require('studio');
-// var studioCluster = require('studio-cluster');
-// Studio.use(studioCluster({}));
+import UserController from '../controllers/UserController';
 
 const router = Router();
 
 const UserComponent = Studio.module('UserComponent');
+
 /* The Base Path for this router is /user you can see it on index.js */
 
-router.post('/account', (req, res) => {
-
-
-    let createUser = UserComponent('createUser');
-
-
-    // createUser().then((value) => {
-    //     res.send(value);
-    // });
-    // setTimeout(function () {
-    //     createUser().then((value) => {
-    //         res.send(value);
-    //     });
-    // }, 1000);
-    // var User = Studio.module('UserComponent');
-    //
-    //
-
-    console.log(req.body);
-    createUser(req.body)
-        .then((response) => {
-            res.status(200).json(response);
-        })
-        .catch((err) => {
-            res.status(500).json(err);
-        })
-});
+router.post('/account', UserController.userCreate);
 
 export default router;
--- Users/components/UserComponent.js
+++ Users/components/UserComponent.js
@@ -1,6 +1,8 @@
 import Studio from 'studio';
 import MessageHandler from '../utils/MessageHandler';
 import User from '../models/User';
+import jwtHandler from '../utils/jwtHandler';
+import bcrypt from 'bcrypt';
 
 
 class UserComponent {
@@ -10,26 +12,42 @@ class UserComponent {
         return User
             .create(userData)
             .then((user) => {
-                return MessageHandler.MessageGenerator("User created succefully", true);
+                return MessageHandler.messageGenerator("User created succefully", true);
             })
             .catch((err) => {
-                if(err.code === 11000)
-                    return MessageHandler.MessageGenerator("The user already exist", false);
+                if (err.code === 11000 || err.code === 11001)
+                    return MessageHandler.messageGenerator("The user already exist", false);
 
                 throw new Error(err);
             });
 
     }
 
-    loginUser() {
+    loginUser(userData) {
 
-        return 'Not yet';
+        return User
+            .findOne({
+                email: userData.email
+            })
+            .then((user) => {
+                if (!user || !bcrypt.compareSync(userData.password, user.password))
+                    return MessageHandler.messageGenerator("The credentials are invalid, please check it out",
+                        false);
+
+                let payload = {"id": user._id};
+
+                return MessageHandler.messageGenerator(jwtHandler.generateAccessToken(payload),
+                    true,'token');
+
+            })
+            .catch((err) => {
+                throw new Error(err);
+            })
     }
 
 }
 //return a new instance from your Microservices component
 var serviceObj = Studio.serviceClass(UserComponent);
 
-
-
 serviceObj.createUser.retry(3);
+serviceObj.loginUser.retry(3);
