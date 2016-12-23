e6262b0142a672d61de2231668dcc3014ecbad42
root <root@Alonso-PC>
2016-12-22 23:58:09 -0400

    Fixing Product batch

--- Products/business/ProductService.js
+++ Products/business/ProductService.js
@@ -1,6 +1,10 @@
 import Product from '../models/product';
 import Studio from 'studio';
 import MessageHandler from '../handler/MessageHandler';
+import co from 'co';
+import _ from 'lodash';
+import Promise from 'bluebird';
+
 const ImageComponent = Studio.module('ImageComponent');
 
 const productBelongsToUser = (ProductData, property) => {
@@ -86,47 +90,28 @@ const store = (productData) => {
          };
 
          const getBatch = (ProductData) => {
-            return co.wrap(function*() {
-               let products = yield Product.find({ iduser : ProductData.iduser });
 
-               console.log(products);
+               return co.wrap(function*() {
 
-            });
+                  let ImageBatch = ImageComponent('getBatchImage');
 
-            //todo, send array of ids to imagecomponent getbatchimages, to retrieve images and set them to the batch
-            // ImageComponent.getbatchimages()
-            //
-            // {
-            // "guids":["5d6ff76c-a1d9-47ea-a89d-1959b702859c",
-            //          "d72a2f54-b7b4-4e61-beb1-8cb7c74aceee",
-            //          "5ba01a4a-d140-4853-b927-254e9df5813e",
-            //          "asdfsdfasdfasdfasdfasdfasdfasd"
-            //          ]
-            // }
-            //
-            // And it will return
-            //
-            //[
-            //  {
-            //    "id": "5d6ff76c-a1d9-47ea-a89d-1959b702859c",
-            //    "SignedUrl": "https://servicecommerce.s3.amazonaws.com/product/75d9edb2e5fd30508d4de879d7af46f805e7dc60ee9abf785bf4f1a5ae4cf19573e45d00?AWSAccessKeyId=AKIAJSLKMUJD7YD3FMDA&Expires=1481988468&Signature=5hhRVGj0MV%2BCWtk5vtvVuG4d8E8%3D  (50KB) "
-            //  },
-            //  {
-            //    "id": "d72a2f54-b7b4-4e61-beb1-8cb7c74aceee",
-            //    "SignedUrl": "https://servicecommerce.s3.amazonaws.com/product/248ae9b5b1ac33078d4eee7fdaaf46aa56b7dc63b3c1ea7852aea6abaf4cf5c628b40106?AWSAccessKeyId=AKIAJSLKMUJD7YD3FMDA&Expires=1481988469&Signature=aOEwHwq8%2BOEhW2cWA4QUIuqR0pU%3D  (49KB) "
-            //  },
-            //  {
-            //    "id": "5ba01a4a-d140-4853-b927-254e9df5813e",
-            //    "SignedUrl": "https://servicecommerce.s3.amazonaws.com/product/75dfbae4b2ab32528d48e829deaf46f755b5dc63ef91ec7858f8f0f9f51fa79273e05706?AWSAccessKeyId=AKIAJSLKMUJD7YD3FMDA&Expires=1481988469&Signature=odDn7PWFy2C4kJx0eMl1Sjwj%2FqI%3D (8KB) "
-            //  }
-            // ]
+                  let products = yield Product.find({ iduser : ProductData.iduser });
+                  let data = {'guids' : _.map(products, product => product._id), 'ObjectType' : 'product'};
+                  return ImageBatch(data).then((images) => {
+                     for (const product of products) {
+                        let img = _.find(images, image => image.id === product._id );
+                        if(img){
+                           product.SignedUrl = img.SignedUrl;
+                        }
+                     }
 
-            return Product.find({ iduser : ProductData.iduser }).populate('offer').select('-__v')
-            .then((products) => {
-               return MessageHandler.messageGenerator(products, true, 'data');
-            }).catch((err) => {
-               return MessageHandler.errorGenerator("Something wrong happened getting product batch", 500);
-            });
+                     return products;
+
+                  }).catch((err) =>{
+                     console.log(err);
+                     return products;
+                  });
+               })();
          };
 
          const assignOffer = (OfferData) => {
