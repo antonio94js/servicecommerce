92d76b8aff3cd21005e4c17718327e25460eff5f
Alonso Salas <alosalasv@gmail.com>
2017-01-12 05:35:56 -0400

    Improving email function

--- Notifications/business/EmailService.js
+++ Notifications/business/EmailService.js
@@ -20,73 +20,75 @@ const sendEmail = (notificationData) => {
     let message = {};
     let subject = {};
 
-    let userData = yield retrieveUserField({credential:data.ownerName,field:'email'});
+    let userData = yield retrieveUserField({credential:data.subjectcredential,field:'email'});
 
-    if(userData){
-      switch (context) {
+    if(!userData) return;
+    switch (context) {
 
-        case 'comment':
+      case 'comment':
 
-          subject = `New question in ${data.publicationName}`;
-          message = `A client has made a new question in your publication ${data.publicationName} \n The question is: \n\n ${data.body}`;
+      subject = `New question in ${data.publicationName}`;
+      message = `A client has made a new question in your publication ${data.publicationName} \n The question is: \n\n ${data.body}`;
 
-        break;
-        case 'response':
+      break;
+      case 'response':
+      subject = `New response in ${data.publicationName}`;
+      message = `The Seller has commented in the publication ${data.publicationName} \n The response is: \n\n ${data.body}`;
 
-        break;
-        // case 'newOrder':
-        //
-        //     break;
-        // case 'cancelOrder':
-        //
-        //     break;
-        // case 'endOrder':
-        //
-        //     break;
-        default:
-        break;
+      break;
+      // case 'newOrder':
+      //
+      //     break;
+      // case 'cancelOrder':
+      //
+      //     break;
+      // case 'endOrder':
+      //
+      //     break;
+      default:
+      break;
 
-      }
-
-      let request = sg.emptyRequest({
-        method: 'POST',
-        path: '/v3/mail/send',
-        body: {
-          personalizations: [
-            {
-              to: [
-                {
-                  email: userData.email,
-                },
-              ],
-              subject: subject,
-            },
-          ],
-          from: {
-            email: SC_EMAIL,
-          },
-          content: [
-            {
-              type: 'text/html',
-              value: message,
-            },
-          ],
-          template_id : "14fe1dfe-29a4-48b4-a92e-b336c1e07177",
-        },
-      });
-
-      sg.API(request)
-      .then(response => {
-        console.log(response.statusCode);
-        // return MessageHandler.messageGenerator('Email sent successfully', true);
-      })
-      .catch(error => {
-        //error is an instance of SendGridError
-        //The full response is attached to error.response
-        console.log(error);
-        // throw MessageHandler.errorGenerator(error, 500);
-      });
     }
+
+    let request = sg.emptyRequest({
+      method: 'POST',
+      path: '/v3/mail/send',
+      body: {
+        personalizations: [
+          {
+            to: [
+              {
+                email: userData.email,
+              },
+            ],
+            subject: subject,
+          },
+        ],
+        from: {
+          email: SC_EMAIL,
+        },
+        content: [
+          {
+            type: 'text/html',
+            value: message,
+          },
+        ],
+        template_id : "14fe1dfe-29a4-48b4-a92e-b336c1e07177",
+      },
+    });
+
+    sg.API(request)
+    .then(response => {
+      console.log(response.statusCode);
+      // return MessageHandler.messageGenerator('Email sent successfully', true);
+    })
+    .catch(error => {
+      //error is an instance of SendGridError
+      //The full response is attached to error.response
+      console.log(error);
+      // throw MessageHandler.errorGenerator(error, 500);
+    });
+
 
   })();
 };
